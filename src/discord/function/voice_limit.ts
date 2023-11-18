fd8f043defbe27b727b20f04c009d26636a92344
partridge <anhduc130703@gmail.com>
2023-11-18 23:51:08 +0700

    add something look dumb

--- src/discord/function/voice_limit.ts
+++ src/discord/function/voice_limit.ts
@@ -1,5 +1,32 @@
-import { ChannelType, VoiceState } from 'discord.js';
+import { ChannelType, EmbedBuilder, GuildMember, VoiceState } from 'discord.js';
 import { cachedVC } from '../bot_client';
+
+const generateEmbed = (member: GuildMember, type: 'CREATED' | 'EDIT') => {
+  const embed = new EmbedBuilder();
+  embed.setAuthor({
+    iconURL: member.displayAvatarURL(),
+    name: member.displayName,
+  });
+  embed.setTitle(
+    type === 'CREATED'
+      ? 'Temporary Channel Created'
+      : 'Temporary Channel Author Changed'
+  );
+  embed.setDescription(`
+  COMMAND:\n</voice bitrate:1175474410312962049> Để chỉnh bitrate cho Channel.\n</voice limit:1175474410312962049> Để chỉnh limit cho Channel.`);
+  embed.addFields([
+    {
+      name: 'Author',
+      value: member.displayName,
+    },
+  ]);
+  embed.setFooter({
+    iconURL: member.guild.client.user.displayAvatarURL(),
+    text: member.guild.client.user.tag,
+  });
+
+  return embed;
+};
 
 const createVoiceChannel = async (voiceState: VoiceState) => {
   const channel = voiceState.channel;
@@ -14,7 +41,10 @@ const createVoiceChannel = async (voiceState: VoiceState) => {
       userLimit: 7,
     });
 
-    await member.voice.setChannel(createdVC);
+    await Promise.all([
+      member.voice.setChannel(createdVC),
+      createdVC.send({ embeds: [generateEmbed(member, 'CREATED')] }),
+    ]);
 
     cachedVC.set(createdVC.id, {
       voiceChannel: createdVC,
@@ -44,9 +74,13 @@ const deleteVoiceChannel = async (voiceState: VoiceState) => {
     const member = vc.members.first();
     if (!member) return;
 
-    await vc.edit({
-      name: `m -> ${member.displayName.toLowerCase()}`,
-    });
+    await vc
+      .edit({
+        name: `m -> ${member.displayName.toLowerCase()}`,
+      })
+      .then((channel) =>
+        channel.send({ embeds: [generateEmbed(member, 'EDIT')] })
+      );
     cachedVC.set(vc.id, {
       voiceChannel: vc,
       authorId: member.id,
