1ebb9bbc2d45a8424102398ebb70d9af2093dca3
partridge <anhduc130703@gmail.com>
2023-11-24 02:42:14 +0700

    reset commands

--- src/discord/command/index.ts
+++ src/discord/command/index.ts
@@ -30,7 +30,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
   try {
     await rest.put(Routes.applicationCommands(process.env.DISCORD_ID), {
-      body: [],
+      body: commands,
     });
 
     console.log(`Loaded ${commands.length} commands`);
